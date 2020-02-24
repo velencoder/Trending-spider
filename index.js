ebdde0fea3c386b05229192cc776261632f7b28c
velen <velencoder@outlook.com>
2020-02-24 19:41:59 +0800

    添加路由

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const Koa = require("koa");
+const Router = require("koa-router");
+const cheerio = require("cheerio");
+const superagent = require("superagent-charset");
+
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx=>{
-  ctx.body ='hello World'
-})
+router.get('/', function(ctx, next) {
+  ctx.body = "Hello World";
+});
 
-app.listen(3000,()=>{
-  console.log("server is running at 3000 port");
-})
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => {
+  console.log("[服务已开启,访问地址为：] http://127.0.0.1:3000/");
+});
