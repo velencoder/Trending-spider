9a99fe3ae845440f4a0f97d40b822b4483ba4df9
velen <velencoder@outlook.com>
2020-02-24 19:53:30 +0800

    引入必要的库

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const Koa = require("koa");
 const Router = require("koa-router");
 const cheerio = require("cheerio");
-const superagent = require("superagent-charset");
+const superagent = require("superagent");
+require('superagent-charset')(superagent)
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/', function(ctx, next) {
-  ctx.body = "Hello World";
+router.get("/", function(ctx, next) {
+  url = "https://github.com/trending";
+  let arr = [1];
+
+  superagent
+    .get(url)
+    .charset("utf-8") //当前页面编码格式
+    .end((err, sres) => {
+      //页面获取到的数据
+      // let html = sres.text,
+      //     $ = cheerio.load(html, {
+      //         decodeEntities: false
+      //     }), //用cheerio解析页面数据
+      //     obj = {};
+      // arr = [];
+      // //下面类似于jquery的操作，前端的小伙伴们肯定很熟悉啦
+      // $(".hotnews ul li").each((index, element) => {
+      //     var $text = $(element).text();
+      //     arr.push($text);
+      // });
+    });
+  ctx.body = arr;
 });
 
 app.use(router.routes()).use(router.allowedMethods());
